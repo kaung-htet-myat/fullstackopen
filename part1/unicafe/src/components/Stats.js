d84107a081f3033dead68251808f7c65c06123c3
Christopher-K-Constantine <kaunghm.09@gmail.com>
2021-09-21 11:40:06 +0630

    table in unicafe

--- part1/unicafe/src/components/Stats.js
+++ part1/unicafe/src/components/Stats.js
@@ -2,26 +2,21 @@ import React from 'react';
 import StatsLine from './StatsLine';
 
 const Stats = ({ stats }) => {
-    let calc = (
-        <tbody>
-            <StatsLine text="Good" value={stats.good} />
-            <StatsLine text="Neutral" value={stats.neutral} />
-            <StatsLine text="Bad" value={stats.bad} />
-            <tr><td>No Feedback Given</td></tr>
-        </tbody>
-    )
+    let calc = <p>No Feedback Given</p>
     let statsBool = (stats.good + stats.neutral + stats.bad) === 0;
 
     if (!statsBool) {
         calc = (
-            <tbody>
-                <StatsLine text="Good" value={stats.good} />
-                <StatsLine text="Neutral" value={stats.neutral} />
-                <StatsLine text="Bad" value={stats.bad} />
-                <tr><td>All: </td><td>{total()}</td></tr>
-                <tr><td>Average: </td><td>{calcAvg()}</td></tr>
-                <tr><td>Positive: </td><td>{calcPos()}</td></tr>
-            </tbody>
+            <table>
+                <tbody>
+                    <StatsLine text="Good" value={stats.good} />
+                    <StatsLine text="Neutral" value={stats.neutral} />
+                    <StatsLine text="Bad" value={stats.bad} />
+                    <tr><td>All: </td><td>{total()}</td></tr>
+                    <tr><td>Average: </td><td>{calcAvg()}</td></tr>
+                    <tr><td>Positive: </td><td>{calcPos()}</td></tr>
+                </tbody>
+            </table>
         )
     }
 
@@ -43,9 +38,7 @@ const Stats = ({ stats }) => {
     return (
         <div>
             <h3>Statistics</h3>
-            <table>
-                {calc}
-            </table>
+            {calc}
         </div>
     );
 }
