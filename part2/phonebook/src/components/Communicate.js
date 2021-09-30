e8b2918fc63c85e1de82a9775a0771f936460a7a
Christopher-K-Constantine <kaunghm.09@gmail.com>
2021-09-30 22:06:45 +0630

    change of rest api

--- part2/phonebook/src/components/Communicate.js
+++ part2/phonebook/src/components/Communicate.js
@@ -1,26 +1,28 @@
 import axios from 'axios'
 
+const baseUrl = "http://localhost:3001/api/persons"
+
 const postNewPerson = (name, number) => {
     const person = {
       name: name,
       number: number,
     }
-    const request = axios.post("http://localhost:3001/persons", person)
+    const request = axios.post(baseUrl, person)
     return request.then(response => response.data)
   }
 
 const getPersons = () => {
-    const request = axios.get("http://localhost:3001/persons")
+    const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
 const deletePerson = (id) => {
-    const request = axios.delete(`http://localhost:3001/persons/${id}`)
+    const request = axios.delete(`${baseUrl}/${id}`)
     return request.then(response => response)
 }
 
 const updatePerson = (changedPerson) => {
-    const request = axios.put(`http://localhost:3001/persons/${changedPerson.id}`, changedPerson)
+    const request = axios.put(`${baseUrl}/${changedPerson.id}`, changedPerson)
     return request.then(response => response.data)
 }
 
